a38251649046a48863d86680d2ba76c968ae2d0b
Stacy <stasi_1992@abv.bg>
2019-01-12 12:50:50 +0200

    cardGame) remove duplicate code

--- cardGame.js
+++ cardGame.js
@@ -83,70 +83,36 @@ function solve(arr){
         }
 
         if (card.length === 3){
-                let cardPower = card[0] + card[1];
-                let cardType = card[2];
+            let cardPower = card[0] + card[1];
+            let cardType = card[2];
 
-                switch (cardPower){
-                    case "2":
-                        cardPower = 2;
-                        break;
-                    case "3":
-                        cardPower = 3;
-                        break;
-                    case "4":
-                        cardPower = 4;
-                        break;
-                    case "5":
-                        cardPower = 5;
-                        break;
-                    case "6":
-                        cardPower = 6;
-                        break;
-                    case "7":
-                        cardPower = 7;
-                        break;
-                    case "8":
-                        cardPower = 8;
-                        break;
-                    case "9":
-                        cardPower = 9;
-                        break;
-                    case "10":
-                        carDPower = 10
-                        break;
-                    case "J":
-                        cardPower = 11;
-                        break;
-                    case "Q":
-                        cardPower = 12;
-                        break;
-                    case "K":
-                        cardPower = 13;
-                        break;
-                    case "A":
-                        cardPower = 14;
-                        break;
-                }
+            switch (cardPower){
+                case "10":
+                    cardPower = 10
+                    break;
+            }
 
-                switch (cardType){
-                    case "S":
-                        cardType = 4;
-                        break;
-                    case "H":
-                        cardType = 3;
-                        break;
-                    case "D":
-                        cardType = 2;
-                        break;
-                    case "C":
-                        cardType = 1;
-                        break;
-                }
+            switch (cardType){
+                case "S":
+                    cardType = 4;
+                    break;
+                case "H":
+                    cardType = 3;
+                    break;
+                case "D":
+                    cardType = 2;
+                    break;
+                case "C":
+                    cardType = 1;
+                    break;
+            }
+            
             multipliedCards = cardPower * cardType;
             sum += multipliedCards;
         }
     }
-    console.log(`${name}: ${sum}`);
+
+        console.log(`${name}: ${sum}`);
     }
 }
 
