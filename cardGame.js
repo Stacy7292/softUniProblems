dc64eba61e5b16fc8e01851a2225529d2e284f53
Stacy <stasi_1992@abv.bg>
2019-01-12 10:26:13 +0200

    cardGame) Process input and store it into a map of sets

---
+++ cardGame.js
@@ -0,0 +1,53 @@
+function solve(arr){
+    let playersAndCards = new Map();
+
+    for (let parts of arr){
+        let part = parts.split(": ");
+        let name = part[0];
+        let deck = part[1];
+        let deckArr = deck.split(", ");
+        let deckSet = new Set();
+
+        for (let i = 0; i < deckArr.length; i++){
+            deckSet.add(deckArr[i]);
+
+        }
+        //console.log(deckSet);
+        playersAndCards.set(name, deckSet);
+    }
+
+    console.log(playersAndCards)
+
+
+
+
+
+}
+
+solve(['Pesho: 2C, 4H, 9H, AS, QS',
+'Slav: 3H, 10S, JC, KD, 5S, 10S',
+'Peshoslav: QH, QC, QS, QD',f
+'Slav: 6H, 7S, KC, KD, 5S, 10C',
+'Peshoslav: QH, QC, JS, JD, JC',
+'Pesho: JD, JD, JD, JD, JD, JD']);
+
+
+
+
+
+
+
+
+/* solve(["Bulgaria -> Sofia -> 500",
+"Bulgaria -> Sopot -> 800",
+"France -> Paris -> 2000",
+"Albania -> Tirana -> 1000",
+"Bulgaria -> Sofia -> 200"]);
+
+solve(["Pesho: 2C, 4H, 9H, AS, QS",
+"Slav: 3H, 10S, JC, KD, 5S, 10S",
+"Peshoslav: QH, QC, QS, QD",
+"Slav: 6H, 7S, KC, KD, 5S, 10C",
+"Peshoslav: QH, QC, JS, JD, JC",
+"Pesho: JD, JD, JD, JD, JD, JD"]);
+*/
