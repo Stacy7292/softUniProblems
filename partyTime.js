db460aa59e87443ea2d90569ea7eb92258ac7e30
Stacy <stasi_1992@abv.bg>
2019-01-13 09:50:17 +0200

    partyTime) created plan

---
+++ partyTime.js
@@ -0,0 +1,7 @@
+// 1. take the first reservation list, until command "PARTY" and add it to a list, called reservations
+// 2. take the second guest list, after command "PARTY", and add it to a new list, called guests;
+// 3. loop through the first list and check if it has the guests from the second list;
+// 4. if not, add them to a new set, called nonVisitors;
+// 5. we separate the set nonVisitors into two new arrays - VIP and Regular;
+// 6. print the size of the set nonVisitors;
+// 7. print VIP and then Regular;
