7e4dce2f90e8029a44c8607675f471ca097ba2c4
Stacy <stasi_1992abv.bg>
2019-01-20 20:03:16 +0200

    inventory) problem solved

---
+++ ObjectAndClasses/inventory.js
@@ -0,0 +1,18 @@
+function solve(arr){
+    let heroes = [];
+
+    for (let inputLine of arr){
+        let [name, level, items] = inputLine.split(" / ");
+        level = +level;
+        items = items.split(", ");
+
+        heroes.push(JSON.stringify({name, level, items}));
+    }
+
+    console.log(`[${heroes.join(", ")}]`)
+
+}
+
+solve(["Isacc / 25 / Apple, GravityGun",
+"Derek / 12 / BarrelVest, DestructionSword",
+"Hes / 1 / Desolator, Sentinel, Antara"]);
