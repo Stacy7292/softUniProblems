35bc61291618907f4ab7730bc21a7230e87e38f8
Stacy <stasi_1992abv.bg>
2019-01-20 12:45:24 +0200

    cat) problem solved

--- ObjectAndClasses/cat.js
+++ ObjectAndClasses/cat.js
@@ -1,5 +1,24 @@
-function solve(arr){
+function solve(arr) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
 
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`)
+        }
+    }
+
+    for (let partsStr of arr) {
+        let partArr = partsStr.split(" ");
+        let name = partArr[0];
+        let age = partArr[1];
+
+        let firstCat = new Cat(name, age);
+
+        firstCat.meow();
+    }
 }
 
 solve(['Mellow 2', 'Tom 5']);
