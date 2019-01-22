c52e80c453df85344af12d346e52d6ff0db1ef8f
Stacy <stasi_1992abv.bg>
2019-01-22 21:05:15 +0200

    flightSchedule) problem solved

---
+++ ObjectAndClasses/flightsSchedule.js
@@ -0,0 +1,41 @@
+function solve(arr){
+    let [initialFlights, changedStatuses, flightStatus ] = arr;
+    flightStatus = flightStatus[0];
+
+    let flights = {};
+
+    for (let inputLine of initialFlights){
+        let [sector, destination] = inputLine.split(" ");
+
+         if (!flights.hasOwnProperty(sector)){
+            flights[sector] = {Destination: destination, Status: "Ready to fly"};
+         }
+    }
+
+    for (let inputLine of changedStatuses){
+        let [sector, changedStatus] = inputLine.split(" ");
+
+        if (flights.hasOwnProperty(sector)){
+            flights[sector].Status = changedStatus;
+        }
+    }
+
+    let allFlights = Object.values(flights).filter((f) => f.Status === flightStatus).forEach((el) => console.log(el));
+
+}
+
+solve([['WN269 Delaware',
+'FL2269 Oregon',
+ 'WN498 Las Vegas',
+ 'WN3145 Ohio',
+ 'WN612 Alabama',
+ 'WN4010 New York',
+ 'WN1173 California',
+ 'DL2120 Texas',
+ 'KL5744 Illinois',
+ 'WN678 Pennsylvania'],
+ ['DL2120 Cancelled',
+ 'WN612 Cancelled',
+ 'WN1173 Cancelled',
+ 'SK430 Cancelled'],
+ ['Cancelled']]);
