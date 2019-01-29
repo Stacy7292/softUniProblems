30a4bb5c3b4803276075c42ec5e8914cdf5c695d
Stacy <stasi_1992abv.bg>
2019-01-29 20:10:55 +0200

    censoredWors) not solved

--- stringProcessing/censoredWords.js
+++ stringProcessing/censoredWords.js
@@ -1,13 +1,17 @@
 function solve(text, word){
-    let result = text.replace(word, repeat(word))
+    let result = text.replace(word, repeat(word));
 
     while (text.includes(word)){
         result = text.replace(word, repeat(word));
     }
-
-   word = repeat(word.length, "*");
+ 
+    length = word.length;
+    repeat = result + "*".repeat(length);
+  // word = repeat(word.length, "*");
 
     console.log(result);
 }
 
 solve("A small sentence with some words", "small");
+
+//A ***** sentence with some words
