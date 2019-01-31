119d322512a4238606d92a081bc22c3f36416e42
Stacy <stasi_1992abv.bg>
2019-01-31 18:59:45 +0200

    stringSubstring) problem solved

---
+++ stringProcessing/stringSubstring.js
@@ -0,0 +1,16 @@
+function solve(word, text) {
+    let words = text.toLowerCase().split(" ");
+    
+    if (words.includes(word)){
+        console.log(word);
+    } else {
+        console.log(`${word} not found!`)
+    }
+}
+
+//solve('javascript',
+//'JavaScript is the best programming language');
+
+solve('python',
+'JavaScript is the best programming language');
+//stellawonnowallets
