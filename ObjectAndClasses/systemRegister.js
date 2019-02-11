6e2377bad664631c3dab27ffc8dda49f6055edcd
Stacy7292 <46375432+Stacy7292@users.noreply.github.com>
2019-02-11 10:04:19 +0200

    Update systemRegister.js

--- ObjectAndClasses/systemRegister.js
+++ ObjectAndClasses/systemRegister.js
@@ -37,7 +37,6 @@ function solve(arr) {
    // console.log(sortedSystemRegister)
 }
 
-//create an object
 
 solve(["SULS | Main Site | Home Page",
 "SULS | Main Site | Login Page",
@@ -53,55 +52,5 @@ solve(["SULS | Main Site | Home Page",
 "Indice | Session | Default Storage",
 "Indice | Session | Default Security"]);
 
-/*
-solve([['WN269 Delaware',
-'FL2269 Oregon',
- 'WN498 Las Vegas',
- 'WN3145 Ohio',
- 'WN612 Alabama',
- 'WN4010 New York',
- 'WN1173 California',
- 'DL2120 Texas',
- 'KL5744 Illinois',
- 'WN678 Pennsylvania'],
- ['DL2120 Cancelled',
- 'WN612 Cancelled',
- 'WN1173 Cancelled',
- 'SK330 Cancelled'],
- ['Ready to fly']]);
 
-solve([['WN269 Delaware',
-'FL2269 Oregon',
- 'WN498 Las Vegas',
- 'WN3145 Ohio',
- 'WN612 Alabama',
- 'WN4010 New York',
- 'WN1173 California',
- 'DL2120 Texas',
- 'KL5744 Illinois',
- 'WN678 Pennsylvania'],
 
- ['DL2120 Cancelled',
- 'WN612 Cancelled',
- 'WN1173 Cancelled',
- 'SK430 Cancelled'],
-
- ['Cancelled']]);
-*/
-/*
-]	{ Destination: 'Alabama', Status: 'Cancelled' }
-{ Destination: 'California', Status: 'Cancelled' }
-{ Destination: 'Texas', Status: 'Cancelled' });
-
-*/
-
-/*
-solve(["Appricot : 20.4",
-"Fridge : 1500",
-"TV : 1499",
-"Deodorant : 10",
-"Boiler : 300",
-"Apple : 1.25",
-"Anti-Bug Spray : 15",
-"T-Shirt : 10"]);
-*/
